f190efb8c3bb443218dd0b8cd47057ac02cb1485
zzy <zzy@holidaycloud.cn>
2014-10-24 13:24:28 +0800

    staticList

---
+++ script/staticList.js
@@ -0,0 +1,30 @@
+/**
+ * Created by zzy on 2014/10/24.
+ */
+db.products.find().forEach(function(product){
+    if(product.productType==0){
+        var price = db.prices.findOne({"product":product._id,"date":{"$gte":Date.now()}});
+        db.staticproducts.save({
+            '_id':product._id,
+            'productType':product.productType,
+            'name':product.name,
+            'introduction':product.introduction,
+            'price':price.price,
+            'images':product.images,
+            'isHot':product.isHot,
+            'isRecommend':product.isRecommend
+        });
+    } else if(product.productType==3){
+        var price = db.prices.findOne({"product":product._id});
+        db.staticproducts.save({
+            '_id':product._id,
+            'productType':product.productType,
+            'name':product.name,
+            'introduction':product.introduction,
+            'price':price.price,
+            'images':product.images,
+            'isHot':product.isHot,
+            'isRecommend':product.isRecommend
+        });
+    }
+});
