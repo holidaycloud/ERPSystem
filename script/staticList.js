d41c790f6686dc3350af9f2c211b7224ae5bd497
zzy <zzy@holidaycloud.cn>
2014-10-24 13:27:23 +0800

    staticList

--- script/staticList.js
+++ script/staticList.js
@@ -4,27 +4,31 @@
 db.products.find().forEach(function(product){
     if(product.productType==0){
         var price = db.prices.findOne({"product":product._id,"date":{"$gte":Date.now()}});
-        db.staticproducts.save({
-            '_id':product._id,
-            'productType':product.productType,
-            'name':product.name,
-            'introduction':product.introduction,
-            'price':price.price,
-            'images':product.images,
-            'isHot':product.isHot,
-            'isRecommend':product.isRecommend
-        });
+        if(price){
+            db.staticproducts.save({
+                '_id':product._id,
+                'productType':product.productType,
+                'name':product.name,
+                'introduction':product.introduction,
+                'price':price.price,
+                'images':product.images,
+                'isHot':product.isHot,
+                'isRecommend':product.isRecommend
+            });
+        }
     } else if(product.productType==3){
         var price = db.prices.findOne({"product":product._id});
-        db.staticproducts.save({
-            '_id':product._id,
-            'productType':product.productType,
-            'name':product.name,
-            'introduction':product.introduction,
-            'price':price.price,
-            'images':product.images,
-            'isHot':product.isHot,
-            'isRecommend':product.isRecommend
-        });
+        if(price){
+            db.staticproducts.save({
+                '_id':product._id,
+                'productType':product.productType,
+                'name':product.name,
+                'introduction':product.introduction,
+                'price':price.price,
+                'images':product.images,
+                'isHot':product.isHot,
+                'isRecommend':product.isRecommend
+            });
+        }
     }
 });
