8d1ce07e38ba6230f12d19443d49bd4dc34df342
zzy <zzy@holidaycloud.cn>
2014-12-03 10:07:05 +0800

    marketing list detail update

--- control/marketingCtrl.js
+++ control/marketingCtrl.js
@@ -2,6 +2,7 @@
  * Created by zzy on 2014/11/29.
  */
 var Marketing = require('./../model/marketing');
+var async = require();
 var MarketingCtrl = function(){};
 MarketingCtrl.save = function(ent,name,content,startDate,endDate,channel,fn){
     var marketing = new Marketing({
@@ -16,4 +17,60 @@ MarketingCtrl.save = function(ent,name,content,startDate,endDate,channel,fn){
        fn(err,res);
     });
 };
+
+MarketingCtrl.list = function(page,pageSize,ent,fn){
+    async.auto({
+        'getList':function(cb){
+            Marketing.find({'ent':ent})
+                .skip(page*pageSize)
+                .limit(pageSize)
+                .exec(function(err,marketings){
+                    cb(err,marketings);
+                });
+        },
+        'getTotalSize':function(cb,results){
+            Marketing.count({'ent':ent},function(err,size){
+                cb(err,size);
+            });
+        }
+    },function(err,results){
+        if(err){
+            fn(err,null);
+        } else {
+            fn(null,{
+                'totalSize':results.getTotalSize,
+                'markets':results.getList
+            });
+        }
+    });
+};
+
+MarketingCtrl.detail = function(id,fn){
+    Marketing.findById(id,function(err,res){
+        fn(err,res);
+    });
+};
+
+MarketingCtrl.update = function(id,name,content,startDate,endDate,channel,fn){
+    var obj = {};
+    if(name){
+        obj.name=name;
+    }
+    if(content){
+        obj.content=content;
+    }
+    if(startDate){
+        obj.startDate=startDate;
+    }
+    if(endDate){
+        obj.endDate=endDate;
+    }
+    if(channel){
+        obj.channel=channel;
+    }
+    Marketing.findByIdAndUpdate(id,{'$set':obj},function(err,res){
+       fn(err,res);
+    });
+};
+
 module.exports = MarketingCtrl;
--- routes/marketing.js
+++ routes/marketing.js
@@ -19,4 +19,45 @@ router.post('/save',function(request,response){
         }
     });
 });
+
+router.get('/list',function(request,response){
+    var ent = request.query.ent;
+    var page = request.query.page||0;
+    var pageSize = request.query.pageSize||25;
+    MarketingCtrl.list(page,pageSize,ent,function(err,res){
+        if(err){
+            response.json({'error':1, 'errMsg':err.message});
+        } else {
+            response.json({'error':0, 'data':res});
+        }
+    });
+});
+
+router.get('/detail',function(request,response){
+    var id = request.query.id;
+    MarketingCtrl.detail(id,function(err,res){
+        if(err){
+            response.json({'error':1, 'errMsg':err.message});
+        } else {
+            response.json({'error':0, 'data':res});
+        }
+    });
+});
+
+router.post('/update',function(request,response){
+    var id = request.body.id;
+    var name = request.body.name;
+    var content = request.body.content;
+    var startDate = request.body.startDate;
+    var endDate = request.body.endDate;
+    var channel = request.body.channel;
+    MarketingCtrl.update(id,name,content,startDate,endDate,channel,function(err,res){
+        if(err){
+            response.json({'error':1, 'errMsg':err.message});
+        } else {
+            response.json({'error':0, 'data':res});
+        }
+    });
+});
+
 module.exports = router;
